36562c163f1d4ef8069cbf8eb6c361e694b9ab32
iooojik <sbobrov760@gmail.com>
2021-03-19 21:25:07 +0300

    lang fix

--- app/src/main/java/com/kirovcompany/bensina/ui/HelloFragment.kt
+++ app/src/main/java/com/kirovcompany/bensina/ui/HelloFragment.kt
@@ -2,8 +2,10 @@ package com.kirovcompany.bensina.ui
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +13,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.kirovcompany.bensina.R
 import com.kirovcompany.bensina.StaticVars
@@ -20,16 +23,19 @@ import java.util.*
 class HelloFragment : Fragment(), View.OnClickListener {
 
     private lateinit var rootView : View
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+        preferences = requireActivity().getSharedPreferences(StaticVars().preferencesName, Context.MODE_PRIVATE)
+
         rootView = inflater.inflate(R.layout.fragment_hello, container, false)
         requireActivity().findViewById<ExtendedFloatingActionButton>(R.id.fab).hide()
         rootView.findViewById<ExtendedFloatingActionButton>(R.id.go_next).setOnClickListener(this)
+        rootView.findViewById<ExtendedFloatingActionButton>(R.id.select_lang).setOnClickListener(this)
 
-        rootView.findViewById<Button>(R.id.russian_locale).setOnClickListener(this)
-        rootView.findViewById<Button>(R.id.english_locale).setOnClickListener(this)
-
+        if (!preferences.getBoolean(StaticVars().preferencesLanguageSelected, false))
+            selectLang()
         return rootView
     }
 
@@ -38,18 +44,40 @@ class HelloFragment : Fragment(), View.OnClickListener {
             R.id.go_next -> {
                 findNavController().navigate(R.id.navigation_addCarInfo)
             }
-            R.id.russian_locale -> {
-                requireActivity().getSharedPreferences(StaticVars().preferencesName, Context.MODE_PRIVATE).edit().
-                        putString(StaticVars().preferencesLanguage, "ru").apply()
-                restartActivity()
-
-            }
-            R.id.english_locale -> {
-                requireActivity().getSharedPreferences(StaticVars().preferencesName, Context.MODE_PRIVATE).edit().
-                putString(StaticVars().preferencesLanguage, "en").apply()
-                restartActivity()
+            R.id.select_lang -> {
+                selectLang()
             }
         }
+    }
+
+    private fun selectLang(){
+        val items = arrayOf("\uD83C\uDDF7\uD83C\uDDFA Русский", "\uD83C\uDDEC\uD83C\uDDE7 English")
+
+        MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Выберите язык | Select language")
+                .setItems(items) { _, which ->
+                    when(which){
+                        0 -> {
+                            setRussianLanguage()
+                        }
+                        1 -> {
+                            setGBLanguage()
+                        }
+                    }
+                    preferences.edit().putBoolean(StaticVars().preferencesLanguageSelected, true).apply()
+                }
+                .setCancelable(false)
+                .show()
+    }
+
+    private fun setGBLanguage(){
+        preferences.edit().putString(StaticVars().preferencesLanguage, "en").apply()
+        restartActivity()
+    }
+
+    private fun setRussianLanguage(){
+        preferences.edit().putString(StaticVars().preferencesLanguage, "ru").apply()
+        //restartActivity()
     }
 
     private fun restartActivity(){
